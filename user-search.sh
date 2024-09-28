0131ef633ff141f0a7b68c3e69937dc07d82dae4
rnfor-pro <nfor.rudolph1@gmail.com>
2024-09-28 12:59:33 -0400

    visual studio demo

--- user-search.sh
+++ user-search.sh
@@ -9,7 +9,6 @@ echo " The user account for $username exist in the system.. "
 echo " The user $username is part of Etech ConsultingDevops course "
 elif ls -d /home/$username/; then
 echo " The directory for $username exist in the system "
-echo " Even though the $username acount does not exist "
 else
 echo " The user directory does not exist in the system "
 echo " The user account for $username does not exist "
